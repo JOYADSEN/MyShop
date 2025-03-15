792e1e3f145d447e514d255a5a82453c56bad9d5
JOYADSEN <hainasjoy@gmail.com>
2025-03-15 14:45:23 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,4 +1,4 @@
-/*import { siteConfig } from '@/lib/config'
+import { siteConfig } from '@/lib/config'
 
 /**
  * 驱动版权
@@ -8,13 +8,9 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
-      <a
-        href='https://wowshoplist.com'
-        className='underline justify-start'>
-        WowShopList Team - 60% of Profits Support UNHCR {siteConfig('VERSION')}
-      </a>
+      © 2025 <a href="/">WowShopList</a> - <a href="/about">60% to UNHCR</a>
       .
     </div>
   )
 }
- */
+
