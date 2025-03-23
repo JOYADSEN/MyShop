2a31d2eaeaf8e87a90622f284ca8c21b2e30e71b
JOYADSEN <hainasjoy@gmail.com>
2025-03-23 11:27:48 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,8 +30,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Unique Rankings', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: 'Fresh Finds', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: 'Charity Impact', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: 'Charity Impact', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: 'Fresh Finds', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
