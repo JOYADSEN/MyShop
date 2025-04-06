1e5fbd752985be6700185629d03ab08fe04a0f22
JOYADSEN <hainasjoy@gmail.com>
2025-04-06 23:19:16 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,11 +6,11 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-    // <div className={`inline text-sm font-serif ${props.className || ''}`}>
-    //   <span className='mr-1'></span>
-    //   <a href="https://wowshoplist.com">WowShopList</a> - <a href="/about">60% to UNHCR</a>
-    //   .
-    // </div>
+    <div className={`inline text-sm font-serif ${props.className || ''}`}>
+      <span className='mr-1'></span>
+      <a href="https://wowshoplist.com">WowShopList</a> - <a href="/about">60% to UNHCR</a>
+      .
+    </div>
   )
 }
 
